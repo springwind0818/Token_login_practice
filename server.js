9b96ca7d8e0d752d09bc0eac217572ad669756a2
최민제 <alswpchl0818@gmail.com>
2026-03-17 17:44:00 +0900

    노드제이에스 4일차 완료

--- server.js
+++ server.js
@@ -17,8 +17,8 @@ const app = express();
 app.use(
   cors({
     origin: [
-      "http://127.0.0.1:[본인의 포트번호]",
-      "http://localhost:[본인의 포트번호]",
+      "http://127.0.0.1:5500",
+      "http://localhost:5500",
     ],
     methods: ["OPTIONS", "POST", "GET", "DELETE"],
     credentials: true,
@@ -41,18 +41,42 @@ app.post("/", (req, res) => {
     res.status(401).send("로그인 실패");
   } else {
     // 1. 유저정보가 있는 경우 accessToken을 발급하는 로직을 작성하세요.(sign)
-    // 이곳에 코드를 작성하세요.
+    // 이곳에 코드를 작성하세요. 
+    const accessToken = jwt.sign(
+      userInfo,
+      secretKey,
+      { expiresIn: 
+        1000000
+       }
+    )
     // 2. 응답으로 accessToken을 클라이언트로 전송하세요. (res.send 사용)
     // 이곳에 코드를 작성하세요.
+    res.send(accessToken);
   }
 });
 
 // 클라이언트에서 get 요청을 받은 경우
 app.get("/", (req, res) => {
-  // 3. req headers에 담겨있는 accessToken을 검증하는 로직을 작성하세요.(verify)
-  // 이곳에 코드를 작성하세요.
-  // 4. 검증이 완료되면 유저정보를 클라이언트로 전송하세요.(res.send 사용)
-  // 이곳에 코드를 작성하세요.
+  try {
+    // 1. 헤더에서 'Bearer <token>' 가져오기
+    const authHeader = req.headers.authorization;
+    
+    if (!authHeader) {
+      return res.status(401).send("토큰이 없어요!");
+    }
+
+    // 2. 'Bearer ' 뒷부분만 잘라내기 (split 사용)
+    const token = authHeader.split(" ")[1]; 
+    
+    // 3. 검증
+    const decodedToken = jwt.verify(token, secretKey);
+    
+    // 4. 성공 시 유저 정보 전송
+    res.send(decodedToken);
+  } catch (err) {
+    // 토큰이 만료되었거나 조작된 경우 에러 처리!
+    res.status(401).send("인증에 실패했어.. 다시 로그인해줘!");
+  }
 });
 
 app.listen(3000, () => console.log("서버 실행!"));
